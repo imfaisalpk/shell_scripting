454c4d6b3e92cd7d601d53ef5e382989a1b66b3c
mfaisal <mfaisalpk@outlook.com>
2017-12-20 01:06:28 +0500

    Variables, Conditionals, input.output, arthimetic calculations

---
+++ myfirstshell.sh
@@ -0,0 +1,73 @@
+# /bin/sh
+# message="Hello, Pakistan";
+# newMessage=$message;
+# echo "Message: $newMessage"
+# echo "Message:" $newMessage "and only Hello"
+# echo "My age is: "24 yrs
+
+# echo 'Message: $newMessage'
+
+# date; pwd; #two commands and comment
+
+# echo "Enter your name: "
+# read -p "Enter your name: " name
+# echo "Your name is: "$name
+
+# read -p "Your first and last name: " firstname lastname;
+# echo "your first name: "$firstname " and last name: "$lastname;
+
+# echo "Arg#1 $1"
+# echo "Arg#2 $2"
+# echo "Arg#3 $3"
+# echo "filename $0"
+# echo "total args: $#"
+# echo "two double quoted $*"
+# echo "two indiviual quoted $@"
+# echo "exit status $?"
+# echo "process id $$"
+# echo "proc number last cmd $!"
+
+# number=5
+# ((output=$number+$number))
+# echo "output: "$output
+
+# ((--number))
+# echo "number:"$number
+
+
+# read -p "your firstname and lastname: " firstname lastname
+# read -p "your age: " age
+
+# if [[ $firstname = "muhammad" && $age -ge 20 ]]; then
+#     echo "you are okay to go!"
+# elif [[  $age > 20 ]]; then    
+#     echo "you are bit mixed"
+# else 
+#     echo "Not allowed!"
+# fi
+
+
+# ERROR
+# read -p "yes or no: " yn
+
+# if [[ "x$yn" = 'xy']];then
+#     echo "yes!"
+# else 
+#     echo "NO."
+# fi
+
+# if test 2 = 5 ; then
+#     echo "yes"
+# else
+#     echo "No"
+# fi
+
+
+
+
+
+
+
+
+
+
